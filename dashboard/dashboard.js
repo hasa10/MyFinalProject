81f6f29ddc35be16d4613326226e1ae9075f62c8
hasa10 <rasitha8000@gmail.com>
2025-02-15 20:39:29 +0530

    finished project

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,64 +1,66 @@
-const shrink_btn = document.querySelector(".shrink-btn");
-const search = document.querySelector(".search");
-const sidebar_links = document.querySelectorAll(".sidebar-links a");
-const active_tab = document.querySelector(".active-tab");
-const shortcuts = document.querySelector(".sidebar-links h4");
-const tooltip_elements = document.querySelectorAll(".tooltip-element");
+const readerWrapper = document.querySelector(".reader-wrapper"),
+form = document.querySelector("form"),
+fileInp = form.querySelector("input"),
+infoText = form.querySelector("p"),
+closeBtn1 = document.querySelector(".close"),
+copyBtn = document.querySelector(".copy");
 
-let activeIndex;
-
-shrink_btn.addEventListener("click", () => {
-  document.body.classList.toggle("shrink");
-  setTimeout(moveActiveTab, 400);
-
-  shrink_btn.classList.add("hovered");
-
-  setTimeout(() => {
-    shrink_btn.classList.remove("hovered");
-  }, 500);
-});
-
-search.addEventListener("click", () => {
-  document.body.classList.remove("shrink");
-  search.lastElementChild.focus();
-});
-
-function moveActiveTab() {
-  let topPosition = activeIndex * 58 + 2.5;
-
-  if (activeIndex > 3) {
-    topPosition += shortcuts.clientHeight;
-  }
-
-  active_tab.style.top = `${topPosition}px`;
+function fetchRequest(file, formData) {
+    infoText.innerText = "Scanning QR Code...";
+    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+        method: 'POST', body: formData
+    }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
+        if(!result) return;
+        document.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
+        readerWrapper.classList.add("active");
+    }).catch(() => {
+        infoText.innerText = "Couldn't scan QR Code";
+    });
 }
 
-function changeLink() {
-  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
-  this.classList.add("active");
-
-  activeIndex = this.dataset.active;
-
-  moveActiveTab();
-}
-
-sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
-
-function showTooltip() {
-  let tooltip = this.parentNode.lastElementChild;
-  let spans = tooltip.children;
-  let tooltipIndex = this.dataset.tooltip;
-
-  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
-  spans[tooltipIndex].classList.add("show");
-
-  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
-}
-
-tooltip_elements.forEach((elem) => {
-  elem.addEventListener("mouseover", showTooltip);
+fileInp.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
 });
 
+copyBtn.addEventListener("click", () => {
+    let text = document.querySelector("textarea").textContent;
+    navigator.clipboard.writeText(text);
+});
+
+form.addEventListener("click", () => fileInp.click());
+closeBtn1.addEventListener("click", () => readerWrapper.classList.remove("active"));
+
+const generatorWrapper = document.querySelector(".generator-wrapper"),
+qrInput = generatorWrapper.querySelector(".form input"),
+generateBtn = generatorWrapper.querySelector(".form button"),
+qrImg = generatorWrapper.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        generatorWrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
 
 
 
@@ -155,3 +157,4 @@ chatInput.addEventListener("keydown", (e) => {
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
